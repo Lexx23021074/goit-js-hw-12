b496697b56fd07cb005eb4d05493f46732e37f20
Taras <lexx23021074@gmail.com>
2026-06-28 13:18:25 +0000

    all done

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,21 +1,23 @@
 import axios from 'axios';
 const API_KEY = '56318160-8874d10fff9d5cc9a89c98525';
 const BASE_URL = 'https://pixabay.com/api/';
+
 /**
  * Здійснює HTTP-запит до Pixabay API
  * @param {string} query - Пошукове слово
  * @param {number} page - Номер сторінки для пагінації
  * @returns {Promise<object>} - Об'єкт data з відповіді бекенду
  */
-export async function getImagesByQuery(query) {
+export async function getImagesByQuery(query, page) {
+  // <--- ОБОВ'ЯЗКОВО ДОДАЙ page СЮДИ!
   const params = {
     key: API_KEY,
     q: query,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
-    page: page, // Додаємо динамічну сторінку
-    per_page: 15, // Обмежуємо кількість до 15 за умовою ТЗ
+    page: page, // Тепер JS знає, що це за page
+    per_page: 15,
   };
   const response = await axios.get(BASE_URL, { params });
   return response.data;
